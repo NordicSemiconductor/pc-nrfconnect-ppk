c274bca2dc693ee21c889a49fc974942eaba1b76
bencefr <bence.frenyo@nordicsemi.no>
2018-02-08 13:25:39 +0100

    fixed chart time labels and restart issue

--- lib/components/Chart.jsx
+++ lib/components/Chart.jsx
@@ -60,7 +60,7 @@ const timestampToLabel = (usecs, index, array) => {
         const first = array[0];
         const last = array[array.length - 1];
         const range = last - first;
-        if ((microseconds - first < range / 8) || (last - microseconds < range / 8)) {
+        if ((usecs - first < range / 8) || (last - usecs < range / 8)) {
             return undefined;
         }
     }
