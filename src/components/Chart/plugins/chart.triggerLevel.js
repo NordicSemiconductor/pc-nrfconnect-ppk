c7148ff5a3d6edd6bdcb618ce91acad84c72b817
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2020-09-11 12:08:34 +0200

    Remove unused function

--- src/components/Chart/plugins/chart.triggerLevel.js
+++ src/components/Chart/plugins/chart.triggerLevel.js
@@ -40,20 +40,6 @@ import colors from '../../colors.scss';
 
 const { gray700: color, white } = colors;
 
-// eslint-disable-next-line func-names
-CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
-    if (w < 2 * r) r = w / 2;
-    if (h < 2 * r) r = h / 2;
-    this.beginPath();
-    this.moveTo(x + r, y);
-    this.arcTo(x + w, y, x + w, y + h, r);
-    this.arcTo(x + w, y + h, x, y + h, r);
-    this.arcTo(x, y + h, x, y, r);
-    this.arcTo(x, y, x + w, y, r);
-    this.closePath();
-    return this;
-};
-
 const plugin = {
     id: 'triggerLevel',
 
