a81110b7f97324e92cb8093799a8c08ebbdf6563
aadnekar <aadne.karstad@gmail.com>
2021-12-15 11:54:15 +0100

    Add GA-event for PPK1 usage

--- src/usageDataActions.js
+++ src/usageDataActions.js
@@ -7,6 +7,7 @@
 const EventAction = {
     BUFFER_SIZE_CHANGED: 'Sampling buffer size changed',
     VOLTAGE_MAX_LIMIT_CHANGED: 'Max voltage limit changed',
+    PPK_1_SELECTED: 'PPK1 device selected',
 };
 
 export default EventAction;
