f98140d63886cfa1f9a1927288811f3c15389a07
aadnekar <aadne.karstad@gmail.com>
2022-04-07 16:03:41 +0200

    Add electron to dev dependencies and add more verbose typing to globals
    timestampToIndex and indexToTimestamp now returns negative value on
    error caused by invalid options state. No handling has currently been
    added to calls of the functions.

--- src/globals.ts
+++ src/globals.ts
@@ -17,7 +17,7 @@ type Options = {
     data: Float32Array;
     bits: Uint16Array | null;
     index: number;
-    timestamp: number | undefined;
+    timestamp: number | undefined | null;
 };
 
 export const options: Options = {
@@ -36,21 +36,34 @@ export const nbDigitalChannels = 8;
  * @param {Number} timestamp timestamp to translate to index
  * @returns {Number} index of sample at provided timestamp
  */
-export const timestampToIndex = timestamp =>
-    options.index -
-    ((options.timestamp - timestamp) * options.samplesPerSecond) / 1e6;
+export const timestampToIndex = (timestamp: number): number => {
+    if (options?.timestamp) {
+        return (
+            options.index -
+            ((options.timestamp - timestamp) * options.samplesPerSecond) / 1e6
+        );
+    }
+    return -1;
+};
 
 /**
  * Translate index of sample array to timestamp
  * @param {Number} index index to translate to timestamp
  * @returns {Number} timestamp of sample at provided index
  */
-export const indexToTimestamp = index =>
-    options.timestamp -
-    ((options.index - index) * 1e6) / options.samplesPerSecond;
+export const indexToTimestamp = (index: number): number => {
+    if (options?.timestamp) {
+        return (
+            options.timestamp -
+            ((options.index - index) * 1e6) / options.samplesPerSecond
+        );
+    }
+    return -1;
+};
 
-export const updateTitle = info => {
-    const title = getCurrentWindow().getTitle().split(':')[0].trim();
-
-    getCurrentWindow().setTitle(`${title}${info ? ':' : ''} ${info || ''}`);
+export const updateTitle = (info: string | undefined) => {
+    const title = remote.getCurrentWindow().getTitle().split(':')[0].trim();
+    remote
+        .getCurrentWindow()
+        .setTitle(`${title}${info ? ':' : ''} ${info || ''}`);
 };
