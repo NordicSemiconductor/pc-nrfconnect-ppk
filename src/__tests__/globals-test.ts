a40c65055f7656b5c7e8f7ffe0a389cc2f783d64
aadnekar <aadne.karstad@gmail.com>
2022-04-21 15:13:03 +0200

    Extract global sample buffer resizing
    Collect functions that mutate the global options object into the same
    file. Add tests to support the changes.

--- src/__tests__/globals-test.ts
+++ src/__tests__/globals-test.ts
@@ -4,7 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { options, timestampToIndex } from '../globals';
+import {
+    adjustDataBufferSize,
+    getSamplingTime,
+    options,
+    setSamplingRates,
+    timestampToIndex,
+} from '../globals';
 
 /**
  * Writes index, timestamp and optionally samplesPerSecond to the global options object.
@@ -39,5 +45,76 @@ describe('timestampToIndex', () => {
         expect(timestampToIndex(options.timestamp as number)).toBe(
             options.index
         );
+    });
+});
+
+describe('getSamplingTime', () => {
+    it('from 1 samplesPerSecond should return 1 million', () => {
+        expect(getSamplingTime(1)).toBe(1e6);
+    });
+
+    it('from 10 samplesPerSecond should return 100k', () => {
+        expect(getSamplingTime(10)).toBe(1e5);
+    });
+
+    it('from 100 samplesPerSecond should return 10k', () => {
+        expect(getSamplingTime(100)).toBe(1e4);
+    });
+
+    it('from 1k samplesPerSecond should return 1k', () => {
+        expect(getSamplingTime(1e3)).toBe(1e3);
+    });
+
+    it('from 10k samplesPerSecond should return 100', () => {
+        expect(getSamplingTime(1e4)).toBe(100);
+    });
+
+    it('from 100k samplesPerSecond should return 10', () => {
+        expect(getSamplingTime(1e5)).toBe(10);
+    });
+});
+
+describe('setSamplingRates', () => {
+    it('to have correct values', () => {
+        setSamplingRates(1e3);
+
+        expect(options.samplesPerSecond).toBe(1e3);
+        expect(options.samplingTime).toBe(getSamplingTime(1e3));
+    });
+});
+
+describe('adjustDataBufferSize', () => {
+    it('does nothing if buffer size has not changed', () => {
+        setSamplingRates(10);
+        adjustDataBufferSize(10);
+        const oldDataReference = options.data;
+
+        adjustDataBufferSize(10);
+        const newDataReference = options.data;
+
+        expect(oldDataReference === newDataReference).toBeTruthy;
+    });
+
+    it('creates a new data buffer if buffer size has changed because of samplingRates are changed', () => {
+        setSamplingRates(10);
+        adjustDataBufferSize(10);
+        const oldDataReference = options.data;
+
+        setSamplingRates(100);
+        adjustDataBufferSize(10);
+        const newDataReference = options.data;
+
+        expect(oldDataReference === newDataReference).toBeFalsy;
+    });
+
+    it('creates a new data buffer if buffer size has changed because of sampling duration is changed', () => {
+        setSamplingRates(10);
+        adjustDataBufferSize(10);
+        const oldDataReference = options.data;
+
+        adjustDataBufferSize(100);
+        const newDataReference = options.data;
+
+        expect(oldDataReference === newDataReference).toBeFalsy;
     });
 });
