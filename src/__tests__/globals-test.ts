b161ee52aa395a692cecf3a159e435ce446fa98f
aadnekar <aadne.karstad@gmail.com>
2022-04-21 14:08:42 +0200

    Add baseline for testing globals file

---
+++ src/__tests__/globals-test.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import { options, timestampToIndex } from '../globals';
+
+/**
+ * Writes index, timestamp and optionally samplesPerSecond to the global options object.
+ * @param {number} index what the current options.index should be
+ * @param {number} [samplesPerSecond] what sample rate to use to synchronise the timestamp
+ * @returns {void}: writes silently to the options object
+ */
+const synchronise_index_and_timestamp = (
+    index: number,
+    samplesPerSecond = 1e5
+): void => {
+    options.index = index;
+    options.samplesPerSecond = samplesPerSecond;
+    options.timestamp = index * samplesPerSecond;
+};
+
+beforeEach(() => {
+    options.data = new Float32Array();
+    options.index = 0;
+    options.timestamp = undefined;
+    options.samplesPerSecond = 1e5;
+});
+
+describe('timestampToIndex', () => {
+    it('should return zero if timestamps are undefined', () => {
+        expect(timestampToIndex(undefined)).toBe(0);
+    });
+
+    it('should return index equal to options.index if argument is undefined', () => {});
+});
