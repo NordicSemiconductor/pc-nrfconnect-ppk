b438e954dff6b7d55738086a378a0a1d8f503b60
aadnekar <aadne.karstad@gmail.com>
2022-07-04 12:10:22 +0200

    Refactor panes.js to typescript

--- src/utils/panes.ts
+++ src/utils/panes.ts
@@ -6,11 +6,15 @@
 
 import { currentPane } from 'pc-nrfconnect-shared';
 
+import { RootState } from '../slices';
+
 export const REAL_TIME = 1;
 export const DATA_LOGGER = 0;
 
-export const isRealTimePane = state => currentPane(state) === REAL_TIME;
-export const isDataLoggerPane = state => currentPane(state) === DATA_LOGGER;
+export const isRealTimePane = (state: RootState) =>
+    currentPane(state) === REAL_TIME;
+export const isDataLoggerPane = (state: RootState) =>
+    currentPane(state) === DATA_LOGGER;
 
-export const paneName = state =>
+export const paneName = (state: RootState) =>
     isRealTimePane(state) ? 'real-time' : 'data-logger';
