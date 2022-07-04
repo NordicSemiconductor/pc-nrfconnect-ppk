5819f88361655b209f9a869bcc719c2834876b45
aadnekar <aadne.karstad@gmail.com>
2022-07-04 13:03:33 +0200

    Refactor Device to typescript

--- src/device/abstractDevice.ts
+++ src/device/abstractDevice.ts
@@ -3,20 +3,21 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-unused-vars -- TODO: only temporary whilst refactoring from javascript */
+/* eslint-disable @typescript-eslint/no-explicit-any -- TODO: only temporary whilst refactoring from javascript */
 
 import EventEmitter from 'events';
 
 import PPKCmd from '../constants';
 
-/* eslint-disable class-methods-use-this */
-
-const getAllPropertyNames = obj => {
+const getAllPropertyNames = (obj: any): any => {
     const proto = Object.getPrototypeOf(obj);
     const inherited = proto ? getAllPropertyNames(proto) : [];
     return [...new Set(Object.getOwnPropertyNames(obj).concat(inherited))];
 };
 
-export function convertFloatToByteBuffer(floatnum) {
+export function convertFloatToByteBuffer(floatnum: number): Uint8Array {
     const float = new Float32Array(1);
     float[0] = floatnum;
     const bytes = new Uint8Array(float.buffer);
@@ -25,22 +26,26 @@ export function convertFloatToByteBuffer(floatnum) {
 
 export default class Device extends EventEmitter {
     currentVdd = 0;
-
     triggerWindowRange = { min: 1, max: 10 };
+    capabilities = {};
 
     constructor() {
         super();
         this.capabilities = {};
         getAllPropertyNames(this)
-            .filter(k => k.startsWith('ppk'))
-            .forEach(k => Object.assign(this.capabilities, { [k]: true }));
+            .filter((k: any) => k.startsWith('ppk'))
+            .forEach((k: any) =>
+                Object.assign(this.capabilities, { [k]: true })
+            );
+        console.log('PPK2 capabilities: ');
+        console.log(this.capabilities);
     }
 
     open() {
         throw new Error('not implemented');
     }
 
-    sendCommand() {
+    sendCommand(...args: PPKCmd[]) {
         throw new Error('not implemented');
     }
 
@@ -70,11 +75,11 @@ export default class Device extends EventEmitter {
         return this.sendCommand([PPKCmd.AverageStop]);
     }
 
-    ppkDeviceRunning(...args) {
+    ppkDeviceRunning(...args: PPKCmd) {
         return this.sendCommand([PPKCmd.DeviceRunningSet, ...args]);
     }
 
-    ppkUpdateRegulator(vdd) {
+    ppkUpdateRegulator(vdd: number) {
         this.currentVdd = vdd;
         // eslint-disable-next-line no-bitwise
         return this.sendCommand([PPKCmd.RegulatorSet, vdd >> 8, vdd & 0xff]);
