33e684d1aae9eee77aea9db5d09d7f774c7206ed
aadnekar <aadne.karstad@gmail.com>
2022-09-02 12:25:16 +0200

    Remove console logs

--- src/actions/deviceActions.ts
+++ src/actions/deviceActions.ts
@@ -124,9 +124,6 @@ export function samplingStart() {
                 ? EventAction.START_REAL_TIME_SAMPLE
                 : EventAction.START_DATA_LOGGER_SAMPLE
         );
-        console.log('SAMPLING START: isRealTime?');
-        console.log(isRealTimePane(getState()));
-
         options.data.fill(NaN);
         if (options.bits) {
             options.bits.fill(0);
--- src/device/types.ts
+++ src/device/types.ts
@@ -18,7 +18,6 @@ export interface SampleValues {
 // Should test the capabilities from either deviceInfo or a device instance to
 // see if device is instance of RTTDevice
 export function isRTTDevice(deviceToTest: any): deviceToTest is RTTDevice {
-    console.log(deviceToTest);
     return deviceToTest.capabilities.jlink;
 }
 
