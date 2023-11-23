580904935b172a52d3a28dd06db6c73ae5f72efd
kylebonnici <kylebonnici@hotmail.com>
2023-11-23 17:20:34 +0100

    feat: code quality

--- src/components/Chart/data/commonBitDataFunctions.ts
+++ src/components/Chart/data/commonBitDataFunctions.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { AmpereState, DigitalChannelState } from './dataTypes';
+import { DigitalChannelState } from './dataTypes';
 
 /**
  * @returns {DigitalChannelState[]} Array with length 4000, containing the BitState for the given timestamp,
@@ -20,20 +20,4 @@ export const createEmptyArrayWithDigitalChannelStates = () =>
                 x: undefined,
                 y: undefined,
             } as DigitalChannelState)
-    );
-
-/**
- * @returns {AmpereState[]} Array with length 4000, containing the AmpereState for the given timestamp,
- * where timestamp is the equivalent x coordinate and ampereState is a number represented on the y-axis.
- *
- * The value <4000> has been chosen as a maximum or sufficient number of pixels to be available in the window at any given time.
- * The number of pixels displayed on the screen is dynamically scaled by averaging over several samples when the user zooms out.
- */
-export const createEmptyArrayWithAmpereState = () =>
-    [...Array(4000)].map(
-        () =>
-            ({
-                x: undefined,
-                y: undefined,
-            } as AmpereState)
     );
