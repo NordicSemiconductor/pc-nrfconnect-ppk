NordicSemiconductor/pc-nrfconnect-ppk

commit 22641920ca3cc626c6e3952470e635cd18766401

fix: bit cache filter algorithm

src/components/Chart/data/dataAccumulator.ts
@@ -205,18 +205,50 @@ const removeDigitalChannelsSamplesOutsideScopes = (
     begin: number,
     end: number
 ) => {
-    if (dataChannel.length > 1) {
-        return [
-            {
-                x: begin,
-                y: dataChannel[0].y,
-            },
-            ...dataChannel.slice(1, dataChannel.length - 2),
-            {
-                x: end,
-                y: dataChannel[dataChannel.length - 1].y,
-            },
-        ];
+    if (dataChannel.length >= 2) {
+        let y = dataChannel[1].y;
+        let x = dataChannel[0].x;
+        let add = false;
+        while (x !== undefined && x < begin) {
+            add = true;
+            y = dataChannel[0].y;
+            dataChannel = dataChannel.slice(1);
+            x = dataChannel[0].x;
+        }
+
+        if (add && x !== begin) {
+            dataChannel = [
+                {
+                    x: begin,
+                    y,
+                },
+                ...dataChannel,
+            ];
+        }
+
+        let i = dataChannel.length - 1;
+        y = dataChannel[i].y;
+        x = dataChannel[i].x;
+        add = false;
+        while (x !== undefined && x > end) {
+            add = true;
+            dataChannel = dataChannel.slice(0, i);
+            i = dataChannel.length - 1;
+            y = dataChannel[i].y;
+            x = dataChannel[i].x;
+        }
+
+        if (add) {
+            dataChannel = [
+                ...dataChannel,
+                {
+                    x: end,
+                    y,
+                },
+            ];
+        }
+
+        return dataChannel;
     }
 
     return [];

commit 81cf2628aa0ba8b876a465d9b4ec98604caa9579

Fix: regression from commit 22641920ca3cc626c6e3952470e635cd18766401

src/components/Chart/data/dataAccumulator.ts
@@ -236,7 +236,7 @@ const removeDigitalChannelsSamplesOutsideScopes = (
         let y = dataChannel[1].y;
         let x = dataChannel[0].x;
         let add = false;
-        while (x !== undefined && x < begin) {
+        while (x !== undefined && x < begin && dataChannel.length >= 2) {
             add = true;
             y = dataChannel[0].y;
             dataChannel = dataChannel.slice(1);
@@ -257,7 +257,7 @@ const removeDigitalChannelsSamplesOutsideScopes = (
         y = dataChannel[i].y;
         x = dataChannel[i].x;
         add = false;
-        while (x !== undefined && x > end) {
+        while (x !== undefined && x > end && dataChannel.length >= 2) {
             add = true;
             dataChannel = dataChannel.slice(0, i);
             i = dataChannel.length - 1;